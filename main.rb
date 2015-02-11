9536dda4363df184e70c27ea6e5e6602087b8d9b
Sebastian Witowski <witowski.sebastian@gmail.com>
2015-02-11 22:40:47 +0100

    It's avoiding obstacles

--- main.rb
+++ main.rb
@@ -1,8 +1,11 @@
 STDOUT.sync = true # DO NOT REMOVE
 require 'set'
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
 
+# Plan:
+# if there is a chance to slow down enemy by 3 or more moves with a wall, build a wall
+# if there is enemy closer to the end than I am and he has 3 or less moves to win, build a wall
+# if I'm closer to the end, move
+# if I don't have walls, move
 
 def init
   # w: width of the board
@@ -36,8 +39,12 @@ end
 
 def can_go?(current_location, direction)
   cx, cy = current_location
-  STDERR.puts "Checking if can go #{direction} from #{current_location}"
-  STDERR.puts "List of walls #{$walls.to_a}"
+  # In case direction is not a string but a coordinate
+  if ! direction.is_a? String
+    direction = coordinates_to_direction(current_location, direction)
+  end
+  # STDERR.puts "Checking if can go #{direction} from #{current_location}"
+  # STDERR.puts "List of walls #{$walls.to_a}"
 
   # If the location is outside of the board, return false
   return false if not in_boundaries?(current_location, direction)
@@ -45,22 +52,114 @@ def can_go?(current_location, direction)
   case direction
   when "RIGHT"
     if $walls.include?([cx + 1, cy, 'V']) || $walls.include?([cx + 1, cy - 1, 'V'])
+      STDERR.puts "Can't go #{direction}, there are following walls: #{$walls.to_a}"
       return false
     end
   when "LEFT"
     if $walls.include?([cx - 1, cy, 'V']) || $walls.include?([cx - 1, cy - 1, 'V'])
+      STDERR.puts "Can't go #{direction}, there are following walls: #{$walls.to_a}"
       return false
     end
   when "UP"
     if $walls.include?([cx, cy - 1, 'H']) || $walls.include?([cx - 1, cy - 1, 'H'])
+      STDERR.puts "Can't go #{direction}, there are following walls: #{$walls.to_a}"
       return false
     end
   when "DOWN"
     if $walls.include?([cx, cy + 1, 'H']) || $walls.include?([cx - 1, cy + 1, 'H'])
+      STDERR.puts "Can't go #{direction}, there are following walls: #{$walls.to_a}"
       return false
     end
   end
   return true
+end
+
+def coordinates_to_direction(current_location, next_location)
+  cx, cy = current_location
+  nx, ny = next_location
+  if nx == cx + 1 && ny == cy
+    return "RIGHT"
+  elsif nx == cx - 1 && ny == cy
+    return "LEFT"
+  elsif ny == cy + 1 and nx == cx
+    return "DOWN"
+  elsif ny == cy - 1 and nx == cx
+    return "UP"
+  end
+  nil
+end
+
+def direction_to_coordinates(current_location, direction)
+  cx, cy = current_location
+  case direction
+  when "RIGHT"
+    return cx + 1, cy
+  when "LEFT"
+    return cx - 1, cy
+  when "UP"
+    return cy + 1, cx
+  when "DOWN"
+    return cy - 1, cx
+  end
+  STDERR.puts "Error in direction_to_coordinates. Incorrect coordinates: \
+               current: #{current_location}, direction: #{direction}"
+  nil
+end
+
+def go_to_target(current_location)
+  next_move, path_size = find_next_location(current_location)
+  next_direction = coordinates_to_direction(current_location, next_move)
+end
+
+def find_next_location(current_location)
+  # Breadth First Search algorithm for path finding
+  # TODO: if it's to slow, replace it with A*
+  frontier = Queue.new
+  frontier << current_location
+  came_from = {}
+  came_from[current_location] = nil
+
+  while not frontier.empty?
+    current = frontier.pop
+    if is_target?(current)
+      target = current
+      break
+    end
+    neighbors(current).each do |neighbor|
+      if not came_from.has_key?(neighbor)
+        frontier << neighbor
+        came_from[neighbor] = current
+      end
+    end
+  end
+
+  # Reconstruct the path
+  current = target
+  path = [current]
+  while current != current_location
+    current = came_from[current]
+    path << current
+  end
+  STDERR.puts "current path: #{path}"
+  path_size = path.size - 1
+  next_move = path.slice(-2) # -2 since -1 is the current_location
+  return next_move, path_size
+end
+
+def is_target?(current_location)
+  cx, cy = current_location
+  tx, ty = $players[$myId]['target']
+  if (tx && tx == cx) || (ty && ty == cy)
+    return true
+  end
+  false
+end
+
+def neighbors(current_location)
+  cx, cy = current_location
+  possible_neighbors = [ [cx + 1, cy], [cx - 1, cy],
+                         [cx, cy + 1], [cx, cy - 1] ]
+  possible_neighbors.select { |target| can_go?(current_location, target) }
 end
 
 def in_boundaries?(current_location, direction)
@@ -95,13 +194,16 @@ def print_decision
   # To debug: STDERR.puts 'Debug messages...'
   # action: LEFT, RIGHT, UP, DOWN or 'putX putY putOrientation' for wall
   # Try to go right and if it's not possible - down and right
-  if can_go?($players[$myId]['current_location'], 'RIGHT')
-    puts 'RIGHT'
-  elsif can_go?($players[$myId]['current_location'], 'UP')
-    puts 'UP'
-  elsif can_go?($players[$myId]['current_location'], 'DOWN')
-    puts 'DOWN'
-  end
+  direction = go_to_target($players[$myId]['current_location'])
+  puts direction
+
+  # if can_go?($players[$myId]['current_location'], 'RIGHT')
+  #   puts 'RIGHT'
+  # elsif can_go?($players[$myId]['current_location'], 'UP')
+  #   puts 'UP'
+  # elsif can_go?($players[$myId]['current_location'], 'DOWN')
+  #   puts 'DOWN'
+  # end
 end
 
 def main
